5083b92eb637efde6c9417d9fd0feea991187b41
NaiveRed <jason841201@gmail.com>
2019-08-28 19:22:17 +0800

    Add more network function.
    0. Add postfix `doki`  to distinguish from the simplified Rainbow
    in Dopamine.
    1. Support dueling network, noisy network
    and specific number of hidden layer. It can be adjusted in .gin.

---
+++ dopamine/unity_domains/noisy_net.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+"""
+Noisy Networks for Exploration
+https://arxiv.org/abs/1706.10295
+"""
+import numpy as np
+import tensorflow as tf
+
+
+def mu_variable(name, shape):
+    """\mu is sampled from independent uniform distribution [-1/sqrt(p), 1/sqrt(p)]
+    (exclude 1/sqrt(p) in this implementation). p is the number of inputs.
+
+    Args:
+        name: Variable name, can not same as existing variables.
+        shape: `list` or array-type, variable shape e.g. [input_unit, output_unit].
+    """
+    return tf.compat.v1.get_variable(name=name,
+                                     initializer=tf.random.uniform(shape,
+                                                                   minval=-1.0 / np.sqrt(shape[0]),
+                                                                   maxval=1.0 / np.sqrt(shape[0]),
+                                                                   dtype=tf.float32),
+                                     trainable=True)
+
+
+def sigma_variable(name, shape, sigma_0=0.5):
+    """
+    Args:
+        name: Variable name, can not same as existing variables.
+        shape: `list` or array-type, variable shape e.g. [input_unit, output_unit].
+        sigma_0: The hyperparameter for initialization, default is 0.5 in paper.
+    """
+    return tf.compat.v1.get_variable(name=name,
+                                     initializer=tf.constant(sigma_0 / np.sqrt(shape[0]),
+                                                             shape=shape,
+                                                             dtype=tf.float32),
+                                     trainable=True)
+
+
+def noisy_linear_layer(name, input_, shape, is_training_ph):
+    """
+    Args:
+        name: Variable name, can not same as existing variables.
+        input_: Input tensor.
+        shape: `list` or array-type, variable shape e.g. [input_unit, output_unit].
+        is_training_ph: `tf.placeholder` with tf.bool type, indicate the training flag for noisy net.
+    """
+    def factorised_fn(x):
+        return tf.sign(x) * tf.sqrt(tf.abs(x))
+
+    def factorised_noise():
+        """
+        Returns:
+            (weight noise, bias noise): Factorised Gaussian noise for weights and bias.
+        """
+        eps_i = tf.random.normal([shape[0]])  # p unit for noise of the inputs
+        eps_j = tf.random.normal([shape[1]])  # q unit for noise of the output
+        f_i = factorised_fn(eps_i)
+        f_j = factorised_fn(eps_j)
+        eps_w = tf.tensordot(f_i, f_j, axes=0)  # Outer product, [shape[0], shape[1]]
+        eps_b = f_j
+        return eps_w, eps_b
+
+    with tf.compat.v1.variable_scope(name):
+        # print('Scope:', tf.contrib.framework.get_name_scope())
+        # Weight variables
+        mu_weight = mu_variable('mu_weight', shape)
+        sigma_weight = sigma_variable('sigma_weight', shape)
+
+        # Bias variables
+        mu_bias = mu_variable('mu_bias', [shape[1]])
+        sigma_bias = sigma_variable('sigma_bias', [shape[1]])
+
+    eps_w, eps_b = tf.cond(is_training_ph, factorised_noise,
+                           (lambda: (tf.zeros(shape), tf.zeros([shape[1]]))))
+
+    linear_weight = mu_weight + sigma_weight * eps_w  # Element-wised product
+    linear_bias = mu_bias + sigma_bias * eps_b  # Element-wised product
+
+    return tf.matmul(input_, linear_weight) + linear_bias
--- dopamine/unity_domains/unity_lib.py
+++ dopamine/unity_domains/unity_lib.py
@@ -1,15 +1,19 @@
 # coding=utf-8
-"""Atari-specific utilities including Atari-specific network architectures.
+"""Unity-specific utilities including some network architectures.
 
-This includes a class implementing minimal Atari 2600 preprocessing, which
-is in charge of:
-  . Emitting a terminal signal when losing a life (optional).
-  . Frame skipping and color pooling.
-  . Resizing the image before it is provided to the agent.
+* rainbow_network: Rainbow network without dueling and noisy net (same as atari_lib)
+* rainbow_network_doki: Rainbow network wtih dueling and noisy net. (not complete)
+* rainbow_mlp_doki: MLP version for vector observations
+
+Modifying from atari_lib.py.
 """
 import gin
 
+import numpy as np
+import tensorflow as tf
+
 from dopamine.unity_domains.unity_wrappers import wrap_unity_env
+from dopamine.unity_domains.noisy_net import noisy_linear_layer
 
 
 @gin.configurable
@@ -17,5 +21,134 @@ def create_unity_environment(game_path=None):
     """Wrap the unity environment
   """
     assert game_path is not None and game_path[-4:] == ".exe", "Game path error."
-    env = wrap_unity_env(game_path, port=9538, use_visual=False)
+    env = wrap_unity_env(game_path, port=9527, use_visual=False)
     return env
+
+
+def nature_dqn_network(num_actions, network_type, state):
+    """The convolutional network used to compute the agent's Q-values.
+
+    Args:
+        num_actions: int, number of actions.
+        network_type: namedtuple, collection of expected values to return.
+        state: `tf.Tensor`, contains the agent's current state.
+
+    Returns:
+        net: _network_type object containing the tensors output by the network.
+    """
+    net = tf.cast(state, tf.float32)
+    net = tf.div(net, 255.)
+    net = tf.contrib.slim.conv2d(net, 32, [8, 8], stride=4)
+    net = tf.contrib.slim.conv2d(net, 64, [4, 4], stride=2)
+    net = tf.contrib.slim.conv2d(net, 64, [3, 3], stride=1)
+    net = tf.contrib.slim.flatten(net)
+    net = tf.contrib.slim.fully_connected(net, 512)
+    q_values = tf.contrib.slim.fully_connected(net, num_actions, activation_fn=None)
+    return network_type(q_values)
+
+
+def rainbow_network(num_actions, num_atoms, support, network_type, state):
+    """The convolutional network used to compute agent's Q-value distributions.
+
+  Args:
+    num_actions: int, number of actions.
+    num_atoms: int, the number of buckets of the value function distribution.
+    support: tf.linspace, the support of the Q-value distribution.
+    network_type: namedtuple, collection of expected values to return.
+    state: `tf.Tensor`, contains the agent's current state.
+
+  Returns:
+    net: _network_type object containing the tensors output by the network.
+  """
+    weights_initializer = tf.contrib.slim.variance_scaling_initializer(factor=1.0 / np.sqrt(3.0),
+                                                                       mode='FAN_IN',
+                                                                       uniform=True)
+
+    net = tf.cast(state, tf.float32)
+    net = tf.div(net, 255.)
+    net = tf.contrib.slim.conv2d(net, 32, [8, 8], stride=4, weights_initializer=weights_initializer)
+    net = tf.contrib.slim.conv2d(net, 64, [4, 4], stride=2, weights_initializer=weights_initializer)
+    net = tf.contrib.slim.conv2d(net, 64, [3, 3], stride=1, weights_initializer=weights_initializer)
+    net = tf.contrib.slim.flatten(net)
+    net = tf.contrib.slim.fully_connected(net, 512, weights_initializer=weights_initializer)
+    net = tf.contrib.slim.fully_connected(net,
+                                          num_actions * num_atoms,
+                                          activation_fn=None,
+                                          weights_initializer=weights_initializer)
+
+    logits = tf.reshape(net, [-1, num_actions, num_atoms])
+    probabilities = tf.contrib.layers.softmax(logits)
+    q_values = tf.reduce_sum(support * probabilities, axis=2)
+    return network_type(q_values, logits, probabilities)
+
+
+@gin.configurable
+def rainbow_doki_mlp(num_actions,
+                     num_atoms,
+                     support,
+                     network_type,
+                     state,
+                     is_training_ph,
+                     num_layer=2,
+                     dueling=True,
+                     noisy=True):
+    """The convolutional network used to compute agent's Q-value distributions.
+
+    Args:
+        num_actions: int, number of actions.
+        num_atoms: int, the number of buckets of the value function distribution.
+        support: tf.linspace, the support of the Q-value distribution.
+        network_type: namedtuple, collection of expected values to return.
+        state: `tf.Tensor`, contains the agent's current state.
+        is_training_ph: `tf.placeholder` with tf.bool type, indicate the training flag for noisy net.
+        num_layer: int, number of hidden layers
+        dueling: bool, enable dueling network.
+        noisy: bool, enable noisy network.
+
+    Returns:
+        net: _network_type object containing the tensors output by the network.
+    """
+
+    net = tf.cast(state, tf.float32)
+    net = tf.contrib.slim.flatten(net)
+    for _ in range(num_layer - 1):
+        net = tf.contrib.slim.fully_connected(net, 512)
+
+    if dueling:
+
+        # Value
+        if noisy:
+            value = noisy_linear_layer('noisy_val_1', net, [512, 512], is_training_ph)
+            value = noisy_linear_layer('noisy_val_2', value, [512, num_atoms], is_training_ph)
+        else:
+            value = tf.contrib.slim.fully_connected(net, 512)
+            value = tf.contrib.slim.fully_connected(value, num_atoms, activation_fn=None)
+
+        # Advantage
+        if noisy:
+            adv = noisy_linear_layer('noisy_adv_1', net, [512, 512], is_training_ph)
+            adv = noisy_linear_layer('noisy_adv_2', adv, [512, num_actions * num_atoms],
+                                     is_training_ph)
+        else:
+            adv = tf.contrib.slim.fully_connected(net, 512)
+            adv = tf.contrib.slim.fully_connected(adv, num_actions * num_atoms, activation_fn=None)
+
+        value = tf.reshape(value, [-1, 1, num_atoms])
+        adv = tf.reshape(adv, [-1, num_actions, num_atoms])
+        logits = value + adv - tf.reduce_mean(adv, axis=1, keepdims=True)
+        probabilities = tf.nn.softmax(logits, axis=2)
+
+    else:
+
+        if noisy:
+            net = noisy_linear_layer(net, [512, 512])
+            net = noisy_linear_layer(net, [512, num_actions * num_atoms])
+        else:
+            net = tf.contrib.slim.fully_connected(net, 512)
+            net = tf.contrib.slim.fully_connected(net, num_actions * num_atoms, activation_fn=None)
+
+        logits = tf.reshape(net, [-1, num_actions, num_atoms])
+        probabilities = tf.contrib.layers.softmax(logits)
+
+    q_values = tf.reduce_sum(support * probabilities, axis=2)
+    return network_type(q_values, logits, probabilities)
